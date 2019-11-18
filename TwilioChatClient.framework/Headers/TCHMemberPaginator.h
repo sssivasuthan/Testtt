6b67f138efebccedc84573ee48a2b161d50baeac
Shiva <sssivasuthan@gmail.com>
2019-11-18 10:03:50 +0530

    Framework added

--- TwilioChatClient.framework/Headers/TCHMemberPaginator.h
+++ TwilioChatClient.framework/Headers/TCHMemberPaginator.h
@@ -1,35 +1,3 @@
-//
-//  TCHMemberPaginator.h
-//  Twilio Chat Client
-//
-//  Copyright (c) 2018 Twilio, Inc. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
-
-#import "TCHConstants.h"
-
-#import "TCHMember.h"
-
-/** The results paginator for members list requests. */
-@interface TCHMemberPaginator : NSObject
-
-/** The items returned by the requested operation, if any.
- 
- @return The items returned by the requested operation, if any.
- */
-- (nonnull NSArray<TCHMember *> *)items;
-
-/** Determine if additional pages are available for the requested operation.
- 
- @return BOOL indicating the presence of a subsequent page of results.
- */
-- (BOOL)hasNextPage;
-
-/** Request the next page of results for the current operation.
- 
- @param completion The paginator completion block.  If no completion block is specified, no operation will be executed.
- */
-- (void)requestNextPageWithCompletion:(nonnull TCHMemberPaginatorCompletion)completion;
-
-@end
+version https://git-lfs.github.com/spec/v1
+oid sha256:40cc66c3af0d989b7ee6f4cb6402c16785e2f6dc744f897913a115a6fd67d780
+size 928
--- TwilioChatClient.framework/Headers/TCHUserDescriptorPaginator.h
+++ TwilioChatClient.framework/Headers/TCHUserDescriptorPaginator.h
@@ -1,35 +1,3 @@
-//
-//  TCHUserDescriptorPaginator.h
-//  Twilio Chat Client
-//
-//  Copyright (c) 2018 Twilio, Inc. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
-
-#import "TCHConstants.h"
-
-#import "TCHUserDescriptor.h"
-
-/** The results paginator for user descriptors list requests. */
-@interface TCHUserDescriptorPaginator : NSObject
-
-/** The items returned by the requested operation, if any.
- 
- @return The items returned by the requested operation, if any.
- */
-- (nonnull NSArray<TCHUserDescriptor *> *)items;
-
-/** Determine if additional pages are available for the requested operation.
- 
- @return BOOL indicating the presence of a subsequent page of results.
- */
-- (BOOL)hasNextPage;
-
-/** Request the next page of results for the current operation.
- 
- @param completion The paginator completion block.  If no completion block is specified, no operation will be executed.
- */
-- (void)requestNextPageWithCompletion:(nonnull TCHUserDescriptorPaginatorCompletion)completion;
-
-@end
+version https://git-lfs.github.com/spec/v1
+oid sha256:de5ce4cf22d832f9d35a1a825c684685489aa515c4decc30afb2bd14b7868bed
+size 977
